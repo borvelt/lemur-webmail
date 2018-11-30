d10bc0391026a2f0bc8e1c359d271e060a6eafe2
borvelt <borvelt@gmail.com>
2018-11-30 03:54:57 +0330

    Add some rules to ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,19 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
-  extends: ['plugins:vue/recommended', 'plugins:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended'
+  ],
 
   plugins: ['vue', 'prettier'],
 
   rules: {
+    'prettier/prettier': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    semi: [2, 'never'],
+    'vue/max-attributes-per-line': 'off'
   }
 }
