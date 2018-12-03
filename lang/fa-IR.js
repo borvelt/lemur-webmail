5dd43bd417370db7a37c258325b59721970321e3
borvelt <borvelt@gmail.com>
2018-12-04 02:14:59 +0330

    setTimeout for loading language and pages.

--- lang/fa-IR.js
+++ lang/fa-IR.js
@@ -1,10 +1,10 @@
-// export default () =>
-//   new Promise(resolve =>
-//     import('~/locales/fa-IR').then(locale =>
-//       setTimeout(() => resolve(locale.default), 1000)
-//     )
-//   )
 export default () =>
   new Promise(resolve =>
-    import('~/locales/fa-IR').then(locale => resolve(locale.default))
+    import('~/locales/fa-IR').then(locale =>
+      setTimeout(() => resolve(locale.default), 1000)
+    )
   )
+// export default () =>
+//   new Promise(resolve =>
+//     import('~/locales/fa-IR').then(locale => resolve(locale.default))
+//   )
--- middleware/router-store.js
+++ middleware/router-store.js
@@ -1,6 +1,6 @@
-// export default ({ store, route }) =>
-//   new Promise(resolve =>
-//     setTimeout(() => resolve(store.commit('route/set', route)), 200)
-//   )
+export default ({ store, route }) =>
+  new Promise(resolve =>
+    setTimeout(() => resolve(store.commit('route/set', route.name)), 200)
+  )
 
-export default ({ store, route }) => store.commit('route/set', route.name)
+// export default ({ store, route }) => store.commit('route/set', route.name)
